f10d9de39584612fedcd3a35126f9b0a7d97fcd1
Lillymuller <eleltaalemu@gmail.com>
2024-08-12 12:33:11 +0000

    update

--- 0x05-react_state/task_0/dashboard/src/Notifications/Notifications.js
+++ 0x05-react_state/task_0/dashboard/src/Notifications/Notifications.js
@@ -1,127 +1,173 @@
-/**
- * @jest-environment jsdom
- */
-import React from "react";
-import App from "./App";
-import Login from "../Login/Login";
-import Header from "../Header/Header";
-import Footer from "../Footer/Footer";
-import Notifications from "../Notifications/Notifications";
-import CourseList from "../CourseList/CourseList";
-import { shallow, mount } from "enzyme";
-import { StyleSheetTestUtils } from "aphrodite";
+import React, { Fragment }from 'react';
+import PropTypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
+import NotificationItem from './NotificationItem';
+import closeIcon from '../assets/close-icon.png';
+import NotificationItemShape from './NotificationItemShape';
 
-beforeEach(() => {
-  StyleSheetTestUtils.suppressStyleInjection();
+class Notifications extends React.Component {
+  constructor(props) {
+    super(props);
+    this.markAsRead = this.markAsRead.bind(this);
+  }
+
+  shouldComponentUpdate(newProps) {
+    return (
+      newProps.listNotifications.length > this.props.listNotifications.length ||
+      newProps.displayDrawer !== this.props.displayDrawer
+    );
+  }
+
+  markAsRead(id) { 
+    console.log(`Notification ${id} has been marked as read`)
+  }
+
+  render() {
+    const { displayDrawer, listNotifications, handleDisplayDrawer, handleHideDrawer } = this.props;
+    const displayMenu = displayDrawer ? styles.noDisplay : styles.menuItem;
+    return (
+      <React.Fragment>
+        <div id='menuItem' className={css(displayMenu)} onClick={handleDisplayDrawer}>
+          Your notifications
+        </div>
+        { displayDrawer &&
+          <div className={css(styles.notifications)}>
+            <button id='closeButton' style={{background: 'transparent', float: 'right'}} aria-label='Close' onClick={handleHideDrawer} >
+              <img src={closeIcon} alt="close-icon" className={css(styles.notificationsButton)} />
+            </button>
+            <p>Here is the list of notifications</p>
+            <ul className={css(styles.notificationsUl)}>
+              {
+                listNotifications.length === 0 &&
+                  (<NotificationItem
+                    value="No new notification for now"
+                  />)
+              }
+              {
+                listNotifications && listNotifications.map((newNote) => (
+                  <NotificationItem
+                    key={newNote.id}
+                    html={newNote.html}
+                    type={newNote.type}
+                    value={newNote.value}
+                    markAsRead={this.markAsRead}
+                    id={newNote.id}
+                  />
+                ))
+              }
+            </ul>
+          </div>
+        }
+      </React.Fragment>
+    );
+  }
+}
+
+const responsive = {
+  small: '@media screen and (max-width: 900px)'
+};
+
+const opacity = {
+  from: {
+    opacity: 0.5,
+  },
+
+  to: {
+    opacity: 1,
+  },
+};
+
+const translateY = {
+  '0%': {
+    transform: "translateY(0)",
+  },
+
+  '50%': {
+    transform: "translateY(-5px)",
+  },
+
+  '75%': {
+    transform: "translateY(5px)",
+  },
+
+  '100%': {
+    transform: "translateY(0)",
+  },
+};
+
+const styles = StyleSheet.create({
+  menuItem: {
+    fontFamily: 'Arial, Helvetica, sans-serif',
+    textAlign: 'right',
+    fontWeight: 'bold',
+    padding: '5px',
+    paddingRight: '20px',
+    backgroundColor: '#fff8f8',
+    display: 'block',
+    ":hover": {
+      cursor: 'pointer',
+      animationName: [opacity, translateY],
+      animationDuration: '1s, 0.5s',
+      animationIterationCount: 3
+    }
+  },
+
+  noDisplay: {
+    display: 'none'
+  },
+
+  notifications: {
+    fontFamily: 'Arial, Helvetica, sans-serif',
+    border: '2px dashed #e1354b',
+    paddingRight: '20px 5px',
+    margin: '5px',
+    position: 'fixed',
+    right: '1%',
+    left: '65%',
+    [responsive.small]: {
+      display: 'block',
+      background: 'white',
+      fontSize: '20px',
+      width: '100vw',
+      height: '100vh',
+      border: 'none',
+      listStyle: 'none',
+      padding: 0,
+      margin: 0,
+      top: 0,
+      right: 0,
+      left: 0,
+    }
+  },
+
+  notificationsButton: {
+    width: '20px',
+    border: 'none',
+    background: 'none',
+    cursor: 'pointer'
+  },
+
+  notificationsUl: {
+    paddingTop: '15px',
+    paddingLeft: '40px',
+    [responsive.small]: {
+      padding: 0
+    }
+  }
 });
-afterEach(() => {
-  StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
-});
 
-describe("App tests", () => {
-  it("renders without crashing", () => {
-    const component = shallow(<App />);
+Notifications.propTypes = {
+  displayDrawer: PropTypes.bool,
+  listNotifications: PropTypes.arrayOf(NotificationItemShape),
+  handleDisplayDrawer: PropTypes.func,
+  handleHideDrawer: PropTypes.func
+};
 
-    expect(component).toBeDefined();
-  });
-  it("should render Notifications component", () => {
-    const component = shallow(<App />);
+Notifications.defaultProps = {
+  displayDrawer: false,
+  listNotifications: [],
+  handleDisplayDrawer: () => {},
+  handleHideDrawer: () => {}
+};
 
-    expect(component.containsMatchingElement(<Notifications />)).toEqual(false);
-  });
-  it("should render Header component", () => {
-    const component = shallow(<App />);
-
-    expect(component.contains(<Header />)).toBe(true);
-  });
-  it("should render Login Component", () => {
-    const component = shallow(<App />);
-
-    expect(component.contains(<Login />)).toBe(true);
-  });
-  it("should render Footer Component", () => {
-    const component = shallow(<App />);
-
-    expect(component.contains(<Footer />)).toBe(true);
-  });
-  it("does not render courselist if logged out", () => {
-    const component = shallow(<App />);
-
-    component.setProps({ isLogedIn: false });
-
-    expect(component.contains(<CourseList />)).toBe(false);
-  });
-  it("renders courselist if logged in", () => {
-    const component = shallow(<App isLoggedIn={true} />);
-
-    expect(component.containsMatchingElement(<CourseList />)).toEqual(false);
-    expect(component.contains(<Login />)).toBe(false);
-  });
-});
-
-describe("When ctrl + h is pressed", () => {
-  it("calls logOut function", () => {
-    const mocked = jest.fn();
-    const wrapper = mount(<App logOut={mocked} />);
-    const event = new KeyboardEvent("keydown", { ctrlKey: true, key: "h" });
-    document.dispatchEvent(event);
-
-    expect(mocked).toHaveBeenCalledTimes(1);
-    wrapper.unmount();
-  });
-
-  document.alert = jest.fn();
-  it("checks that alert function is called", () => {
-    const wrapper = mount(<App />);
-    const spy = jest.spyOn(window, "alert");
-    const event = new KeyboardEvent("keydown", { ctrlKey: true, key: "h" });
-    document.dispatchEvent(event);
-
-    expect(spy).toHaveBeenCalled();
-    spy.mockRestore();
-    wrapper.unmount();
-  });
-
-  it('checks that the alert is "Logging you out"', () => {
-    const wrapper = mount(<App />);
-    const spy = jest.spyOn(window, "alert");
-    const event = new KeyboardEvent("keydown", { ctrlKey: true, key: "h" });
-    document.dispatchEvent(event);
-
-    expect(spy).toHaveBeenCalledWith("Logging you out");
-    jest.restoreAllMocks();
-    wrapper.unmount();
-  });
-  document.alert.mockClear();
-});
-
-it("Has default state for displayDrawer false", () => {
-  const wrapper = shallow(<App />);
-  expect(wrapper.state().displayDrawer).toEqual(false);
-});
-
-it("displayDrawer changes to true when calling handleDisplayDrawer", () => {
-  const wrapper = shallow(<App />);
-  expect(wrapper.state().displayDrawer).toEqual(false);
-
-  const instance = wrapper.instance();
-
-  instance.handleDisplayDrawer();
-
-  expect(wrapper.state().displayDrawer).toEqual(true);
-});
-
-it("displayDrawer changes to false when calling handleHideDrawer", () => {
-  const wrapper = shallow(<App />);
-  expect(wrapper.state().displayDrawer).toEqual(false);
-
-  // const instance = wrapper.instance();
-
-  wrapper.instance().handleDisplayDrawer();
-
-  expect(wrapper.state().displayDrawer).toEqual(true);
-
-  wrapper.instance().handleHideDrawer();
-
-  expect(wrapper.state().displayDrawer).toEqual(false);
-});
+export default Notifications;
